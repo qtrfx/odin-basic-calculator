c6fb9f14aa340337448af6008c8fd25f533efd62
qtrfx <24359705+qtrfx@users.noreply.github.com>
2024-10-16 19:48:48 +0200

    feat: add function to evaluate valid expressions

--- script.js
+++ script.js
@@ -15,6 +15,25 @@ function multiply(a, b) {
 function divide(a, b) {
   return b === 0 ? "No dividing by zero :madge:" : a / b;
 }
+function evaluateExpression(operands) {
+  const returnResult = {
+    valid: true,
+    msg: "",
+  };
+  operands.every((operand) => {
+    if (operand.match(".")[0] > 1) {
+      returnResult.valid = false;
+      returnResult.msg += "Invalid Syntax";
+      return false;
+    }
+  });
+  if (returnResult.valid) {
+    returnResult.msg = "All good!";
+  }
+
+  return returnResult;
+}
+
 function splitExpression(expression) {
   const operatorRegex = /[\+\*\-\/]/;
   const [firstOperand, secondOperand] = expression
