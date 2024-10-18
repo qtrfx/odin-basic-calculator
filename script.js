4f6373181682c5cec75b83fc3834aa8fd348fa3a
qtrfx <24359705+qtrfx@users.noreply.github.com>
2024-10-18 03:24:50 +0200

    feat: add button click handling

--- script.js
+++ script.js
@@ -1,101 +1,38 @@
-let displayValue;
+const keyPad = document.querySelector(".keypad");
+const calculatorDisplay = document.querySelector(".calculator-display");
+let displayValue = "";
 
-function add(a, b) {
-  return a + b;
-}
+keyPad.addEventListener("click", handleClick);
 
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return b === 0 ? "No dividing by zero :madge:" : a / b;
-}
-
-function calculate(expression) {
-  const [firstOperand, secondOperand, operator] = splitExpression(expression);
-  const evalResult = evaluateExpression([firstOperand, secondOperand]);
-
-  if (!evalResult.valid) {
-    return evalResult.msg;
-  } else {
-    let result;
-
-    switch (operator) {
-      case "+":
-        result = add(firstOperand, secondOperand);
-        break;
-      case "-":
-        result = subtract(firstOperand, secondOperand);
-        break;
-      case "/":
-        result = divide(firstOperand, secondOperand);
-        break;
-      case "*":
-        result = multiply(firstOperand, secondOperand);
-        break;
-    }
-    return parseFloat(result);
+function handleClick(event) {
+  if (displayValue === "Syntax Error") {
+    displayValue = "";
   }
-  // evaluate that expression is valid
-  // split expression into its operands
-  // extract operator from expression
-  // switch with operator that calls math function with operands as arguments
-}
-
-function handleClick({ target: { value: keyValue } }) {
-  switch (keyValue) {
-    case "AC":
-      // Call function that clears display
-      break;
-
-    // Fall through on similar cases
-    case "/":
-    case "*":
+  switch (event.target.innerText) {
     case "+":
     case "-":
-      // Call function that evaluates if the operator should be added
-      break;
-    case ".":
-      // Call function that evaluates if a period should be added
+    case "/":
+    case "*":
+      handleOperand(event.target.innerText);
       break;
     case "=":
-      // Try to evaluate expression with function;
-      calculate(displayValue);
+      handleCalculate(displayValue);
+      break;
+    case "AC":
+      resetDisplay();
+      break;
     default:
-    // Call function that adds key value to displayvalue
+      if (event.target.classList.contains("keypad-key")) {
+        handleNumber(event.target.innerText);
+      }
   }
 }
 
-function evaluateExpression(operands) {
-  const returnResult = {
-    valid: true,
-    msg: "",
-  };
-  operands.every((operand) => {
-    if (operand.match(".")[0] > 1) {
-      returnResult.valid = false;
-      returnResult.msg += "Invalid Syntax";
-      return false;
-    }
-  });
-  if (returnResult.valid) {
-    returnResult.msg = "All good!";
-  }
-
-  return returnResult;
+function handleNumber(number) {
+  displayValue += number;
+  updateDisplay(displayValue);
 }
 
-function splitExpression(expression) {
-  const operatorRegex = /[\+\*\-\/]/;
-  const [firstOperand, secondOperand] = expression
-    .split(operatorRegex)
-    .map((e) => parseFloat(e));
-  const operator = expression.match(operatorRegex)[0];
-
-  return [firstOperand, secondOperand, operator];
+function updateDisplay(newValue) {
+  calculatorDisplay.innerText = newValue;
 }
